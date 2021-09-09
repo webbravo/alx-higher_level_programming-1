053a2730cd771c529e2b8f08c171f2320503778e
firdaushassansalim <firdaushassan033@gmail.com>
2021-09-09 16:02:42 +0300

    task 1 completed

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
+
+    a = 10
+    b = 5
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
