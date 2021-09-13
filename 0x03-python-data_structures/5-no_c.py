fd931aee5ba9f95384f8496b1b245aaa6083ed18
firdaushassansalim <firdaushassan033@gmail.com>
2021-09-13 11:02:30 +0300

    fixed bug

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 
 def no_c(my_string):
-    copy_string = [x for x in my_string if x != 'c' and x != 'C']
-    return("".join(copy_string))
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
